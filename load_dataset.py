829c7f7bd34fc6e9a65653547af58c72321f335f
sebastian2608 <sebastiangomezahumada@gmail.com>
2024-06-28 14:09:36 -0500

    Structure migrated to drive structure, fixed load_dataset function

--- load_dataset.py
+++ load_dataset.py
@@ -9,12 +9,14 @@ def load_dataset(dataset_path):
     imgs = []
     labels = []
     names = []
+    i=0
     for directory in glob.glob(os.path.join(dataset_path,"*")):
         print("Loading", directory)
         for file in tqdm(glob.glob(os.path.join(directory, '*.png'))):
 
             img = imread(file,as_gray=True)
             imgs.append(resize(img, (162,300), anti_aliasing=True))
-            labels.append(os.path.split(directory)[1][0])
-            names.append(os.path.split(directory)[1][1:])
+            labels.append(i)
+            names.append(os.path.split(directory)[1])
+        i+=1
     return np.array(imgs), np.array(labels), names
